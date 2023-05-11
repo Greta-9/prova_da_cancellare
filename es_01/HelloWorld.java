bceb7593c69a526b005d8888177e6d0f1ec5b966
Greta-9 <gre.difabio@gmail.com>
2023-05-11 12:01:18 +0200

    es_01 vers 1.0

---
+++ es_01/HelloWorld.java
@@ -0,0 +1,15 @@
+/* Esercitazione "HELLOWORLD"
+ * Test della configurazione dell'area di lavoro
+ */
+public class HelloWorld { //ricordarsi che il nome della classe deve corrispondere al nome del file
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
+/* note
+ * abbiamo rinunciato al suggerimento del tipo di variabile che ci veniva proposto dal sistema
+ * cambiando le impostazioni: "Editor: Inlay Hints: Disabled"
+ * 
+ * è possibile attivare la formattazione automatica attraverso "> tasto detro > formatta documento"
+ * la formattazione è importante per potersi orientare al meglio all'interno del codice
+ */
